f022205a8f81ac58d69ac57c64f5044c5b2e1448
MostafaRaafat91 <eng2mega@gmail.com>
2023-08-12 08:36:05 -0700

    coding last number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,12 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	/* Print the last digit of n */
-	
 	printf("Last digit of %d is ", n % 10);
-	
 	/* Check the last digit and print the corresponding message */
-	
 	if ((n % 10) > 5)
 	{
 		printf("and is greater than 5\n");
@@ -37,6 +33,5 @@ int main(void)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
-	
-	return (0);
+return (0);
 }
