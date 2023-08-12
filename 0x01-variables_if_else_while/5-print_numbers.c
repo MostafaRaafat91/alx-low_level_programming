815f25c8e3b5d3bc8991a22fe82ca8a554fd5374
MostafaRaafat91 <eng2mega@gmail.com>
2023-08-12 10:43:42 -0700

    coding print alphabet

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random n to the variable each time 
+ * and print the last digit of the n
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
