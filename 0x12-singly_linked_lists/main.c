0fb97f2d5f93304d093f4861f79ec76d2a3a7114
MostafaRaafat91 <eng2mega@gmail.com>
2023-09-25 13:12:37 -0700

    fixing a correction

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -22,28 +22,28 @@ int main(void)
 
 {
 
-  list_t *head;
+	    list_t *head;
 
 
 
-  head = NULL;
+	        head = NULL;
 
-  add_node_end(&head, "Bob");
+		    add_node_end(&head, "Bob");
 
-  add_node_end(&head, "&");
+		        add_node_end(&head, "&");
 
-  add_node_end(&head, "Kris");
+			    add_node_end(&head, "Kris");
 
-  add_node_end(&head, "love");
+			        add_node_end(&head, "love");
 
-  add_node_end(&head, "asm");
+				    add_node_end(&head, "asm");
 
-  print_list(head);
+				        print_list(head);
 
-  free_list(head);
+					    free_list(head);
 
-  head = NULL;
+					        head = NULL;
 
-  return (0);
+						    return (0);
 
 }
