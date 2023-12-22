24ebf996cba58c0ba0d768f0b90fab08adb458b9
MostafaRaafat91 <eng2mega@gmail.com>
2023-12-22 10:48:15 -0800

    issues fixed of hash codes

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hastable to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current_node;
+	unsigned long int i;
+	int printed = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			if (printed)
+				printf(", ");
+
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			printed = 1;
+
+			current_node = current_node->next;
+		}
+	}
+
+	printf("}\n");
+}
